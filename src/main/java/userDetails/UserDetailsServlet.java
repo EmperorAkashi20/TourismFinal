131625372a3a4315a1bac56ff178b303e07eb33f
EmperorAkashi20 <rishabhsethiadsce@gmail.com>
2021-07-02 17:46:37 +0530

    index.jsp added

---
+++ src/main/java/userDetails/UserDetailsServlet.java
@@ -0,0 +1,109 @@
+package userDetails;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.userdetails.UserDetails;
+
+import database.UserDatabase;
+
+/**
+ * Servlet implementation class UserDetails
+ */
+@WebServlet("/UserDetails")
+public class UserDetailsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDatabase userDatabase;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserDetailsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		userDatabase = new UserDatabase();
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String password = request.getParameter("password");
+        //String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        UserDetails userDetails = new UserDetails();
+        userDetails.setFirstName(firstName);
+        userDetails.setLastName(lastName);
+        userDetails.setContact(contact);
+        userDetails.setPassword(password);
+
+        try {
+            userDatabase.registerUser(userDetails);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("employeedetails.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
